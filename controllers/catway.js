cf2680bb3c835bc4a7a815f47bf962d3e1d75b60
Tiago Machado <tiago.machado.1987@gmail.com>
2024-11-10 08:59:07 +0100

    add reservations

--- controllers/catway.js
+++ controllers/catway.js
@@ -3,20 +3,13 @@ const Reservations = require('../models/reservations');
 const moment = require('moment');
 
 
-// exports.getHome = (req, res) => {
-//   res.render('catways/index',{
-//     pageTitle: 'Accueil',
-//     path: '/'
-//   });
-// };
-
 exports.getCatways = async (req, res) => {
   try{
     const catways = await Catway.find().sort({ _id: "asc" });
     const reservations = await Reservations.find();
     const error = req.flash('error');
     const success = req.flash('success');
-  
+
     res.render('catways/catways-list', {
       pageTitle: 'Catways list',
       catways: catways,
@@ -24,7 +17,7 @@ exports.getCatways = async (req, res) => {
       path: '/catways',
       error: error,
       success: success,
-      moment: moment
+      moment: moment,
     })  
   }
   catch (error) {
@@ -40,15 +33,16 @@ exports.getCatways = async (req, res) => {
 
 exports.getOneCatway = async (req, res) => {
   try {
-    const catway = await Catway.findById(req.params.id);
-    const reservations = await Reservations.find({ catwayNumber: catway.catwayNumber });
+    const catwayNumber = req.params.id;
+    const catway = await Catway.find({ catwayNumber: catwayNumber });
+    const reservations = await Reservations.find({ catwayNumber: catwayNumber });
     if (!catway) {
       req.flash('error', 'Ce Catway n\'existe pas');
       return res.redirect('/catways/list');
     }
     res.render('catways/catway-detail', {
       pageTitle: 'Detail du Catway',
-      catway: catway,
+      catway: catway[0],
       reservations: reservations,
       moment: moment,
       path: '/catways',
@@ -69,18 +63,28 @@ exports.getOneCatway = async (req, res) => {
 
 exports.getAddCatway = async (req, res) => {
   const error = req.flash('error');
-
+  const success = req.flash('success');
+  const catways = await Catway.find();
+  const reservations = await Reservations.find();
+  const nextCatway = catways.length + 1;
+  
   try {
-    console.log(error);
     res.render('catways/add-catway', {
       pageTitle: 'Ajout d\'un Catway',
       path: '/catways',
       error: error,
+      success: success,
       editMode: false,
+      nextCatway: nextCatway,
+      catways: catways,
+      reservations: reservations,
+      moment: moment
     })  
   }
   catch(error){
     console.log(error);
+    req.flash('error', 'Une erreur s\'est produite lors de l\'obtention des Catways');
+    return res.redirect('/catways/list');
   }
 };
 
@@ -118,7 +122,10 @@ exports.getEditCatway = async (req, res) => {
   const editMode = req.query.edit;
   try{
     const catway = await Catway.findById(req.params.id);
+    const catways = await Catway.find();
+    const reservations = await Reservations.find();
     const error = req.flash('error');
+    const success = req.flash('success');
     if(!catway){
       req.flash('error', 'Ce Catway n\'existe pas');
       return res.redirect('/catways/list');
@@ -128,7 +135,11 @@ exports.getEditCatway = async (req, res) => {
       path: '/catways',
       editMode: editMode,
       catway: catway,
-      error: error
+      error: error,
+      moment: moment,
+      success: success,
+      catways: catways,
+      reservations: reservations
     });
   }
   catch(error){
@@ -142,7 +153,7 @@ exports.replaceCatway = async (req, res) => {
 
   try {
     // Verifica se todos os campos foram preenchidos
-    if (!catway_number || !type || !catway_state) {
+    if (!catway_number || !type.trim() || !catway_state.trim()) {
       req.flash('error', 'Veuillez renseigner tous les champs');
       return res.redirect('/catways/get-edit/' + req.params.id + '?edit=true');
     }
@@ -155,23 +166,17 @@ exports.replaceCatway = async (req, res) => {
       return res.redirect('/catways/list');
     }
 
-    // Verifica se o catwayNumber já existe em outro documento
-    const duplicateCatway = await Catway.findOne({ catwayNumber: catway_number });
-    if (duplicateCatway) {
-      req.flash('error', 'Ce catway existe déjà');
-      return res.redirect('/catways/get-edit/' + req.params.id + '?edit=true');
-    }
+    //Verifica se o catwayNumber já existe em outro documento
+    const filter = { catwayNumber: catway_number };
+    const update = {
+      catwayNumber: catway_number,
+      type: type,
+      catwayState: catway_state
+    };
 
-    // Atualiza os campos do documento
-    catwayDoc.catwayNumber = catway_number;
-    catwayDoc.type = type;
-    catwayDoc.catwayState = catway_state;
-
-    await catwayDoc.save();
-
+    await Catway.findOneAndUpdate(filter, update, { new: true });
     req.flash('success', 'Catway modifié avec succès');
-    return res.redirect('/catways/list');
-  
+    return res.redirect('/catways/get-edit/' + req.params.id + '?edit=true');
 
   } catch (error) {
     console.log(error);
@@ -190,18 +195,15 @@ exports.updateCatway = async (req, res) => {
       return res.redirect('/catways/get-catway/'  + req.params.id);
     }
 
-    // Atualiza apenas os campos enviados no corpo da requisição
-    const updatedCatway = await Catway.findByIdAndUpdate(
-      id,
-      { $set: req.body },  // Utiliza $set para atualizar apenas os campos específicos
-      { new: true, runValidators: true }
-    );
+    const updatedCatway = await Catway.find({ catwayNumber: id });
 
     if (!updatedCatway) {
       req.flash('error', 'Ce Catway n\'existe pas');
       return res.redirect('/catways/list');
     }
 
+    updatedCatway[0].catwayState = state;
+    await updatedCatway[0].save();
     req.flash('success', 'État modifié avec succès');
     return res.redirect('/catways/get-catway/'  + req.params.id);
 
@@ -214,15 +216,24 @@ exports.updateCatway = async (req, res) => {
 
 exports.deleteCatway = async (req, res) => {
   try {
-    const deletedCatway = await Catway.findByIdAndDelete(req.params.id);
+    
+    const reservations = await Reservations.find({ catwayNumber: req.params.id });
+    if (reservations.length > 0) {
+      req.flash('error', 'Ce Catway contient des reservations');
+      return res.redirect('/catways/get-catway/' + req.params.id);
+    }
+
+    const deletedCatway = await Catway.findOneAndDelete({catwayNumber: req.params.id});
     if (!deletedCatway) {
       req.flash('error', 'Ce Catway n\'existe pas');
       return res.redirect('/catways/list');
     }
+
     req.flash('success', 'Catway supprimé avec succès');
     return res.redirect('/catways/list');
+
   } catch (error) {
-      req.flash('error', 'Une erreur s\'est produite lors de la suppression du Catway');
-      return res.redirect('/catways/list');
+    req.flash('error', 'Une erreur s\'est produite lors de la suppression du Catway');
+    return res.redirect('/catways/list');
   }
 };
--- controllers/reservations.js
+++ controllers/reservations.js
@@ -1,82 +1,184 @@
+const moment = require('moment');
+
 const Reservation = require('../models/reservations');
 
-// Obter todas as reservas
-exports.getReservations = async (req, res) => {
-  try {
-    const reservations = await Reservation.find();
-    res.json(reservations);
-  } catch (error) {
-    console.error(error);
-    res.status(400).json({ error: error.message });
-  }
-}
-
-
-// Obter todas as reservas de um catway específico
 exports.getReservationsByCatway = async (req, res) => {
-  const { catwayNumber } = req.params;
+  const { id } = req.params;
+  const error = req.flash('error');
+  const success = req.flash('success');
+
   try {
-    const reservations = await Reservation.find({ catwayNumber: catwayNumber });
-
+    const reservations = await Reservation.find({ catwayNumber: id});
     if (!reservations.length) {
-      return res.status(404).json({ message: 'Nenhuma reserva encontrada para este catway.' });
+      req.flash('error', 'Aucunne reservation pour ce catway');
+      return res.redirect('/catways/get-catway/' + id);
     }
-
-    res.status(200).json(reservations);
+    return res.render('reservations/reservation-by-catway', {
+      reservations: reservations,
+      path: '/reservations',
+      pageTitle: 'Reservations',
+      moment: moment,
+      catwayNumber: id,
+      error: error,
+      success: success
+    });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Erro ao obter reservas.' });
+    req.flash('error', 'Erreur dans l\'obtention des réservations');
+    return res.redirect('/catways/get-catway/' + id);
   }
 };
 
-// Obter uma reserva específica de um catway específico
-exports.getReservationByCatwayAndId = async (req, res) => {
-  const { catwayNumber, idReservation } = req.params;
+exports.getAddReservation = async (req, res) => {
+  const error = req.flash('error');
+  const success = req.flash('success');
+  const catwayNumber = req.params.id;
+  const reservations = await Reservation.find();
   try {
-    const reservation = await Reservation.findOne({
+    res.render('reservations/add-reservation', {
+      pageTitle: 'Ajout réservation',
+      path: '/reservations',
+      error: error,
+      success: success,
       catwayNumber: catwayNumber,
-      _id: idReservation,
+      reservations: reservations,
+      moment: moment
+    })  
+  }
+  catch(error){
+    console.log(error);
+    req.flash('error', 'Une erreur s\'est produite lors de l\'ajout de la reservation');
+    return res.redirect('/catways/list');
+  }
+};
+
+exports.addReservation = async (req, res) => {
+  const { client_name, boat_name, check_in, check_out } = req.body;
+  const catway_number = req.params.id;
+
+  try {
+    // Verifica se todos os campos obrigatórios estão preenchidos
+    if (!catway_number || !client_name || !boat_name || !check_in || !check_out) {
+      req.flash('error', 'Veuillez renseigner tous les champs');
+      return res.redirect('/catways/' + catway_number + '/reservations/get-add');
+    }
+
+    // Converte as datas para objetos Date
+    const checkInDate = new Date(check_in);
+    const checkOutDate = new Date(check_out);
+
+    // Verifica se as datas são válidas
+    if (isNaN(checkInDate.getTime()) || isNaN(checkOutDate.getTime())) {
+      req.flash('error', 'Dates invalides.');
+      return res.redirect('/catways/' + catway_number + '/reservations/get-add');
+    }
+
+    // Verifica se já existe uma reserva que se sobreponha às datas fornecidas
+    const existingReservation = await Reservation.findOne({
+      catwayNumber: catway_number,
+      $or: [
+        {
+          checkIn: { $lt: checkOutDate },
+          checkOut: { $gt: checkInDate }
+        }
+      ]
     });
 
-    if (!reservation) {
-      return res.status(404).json({ message: 'Reserva não encontrada para este catway.' });
+    if (existingReservation) {
+      req.flash('error', 'Une réservation existe déjà pour cette période.');
+      return res.redirect('/catways/' + catway_number + '/reservations/get-add');
     }
 
-    res.status(200).json(reservation);
+    // Cria e salva a nova reserva
+    const reservation = new Reservation({
+      catwayNumber: catway_number,
+      clientName: client_name,
+      boatName: boat_name,
+      checkIn: checkInDate,
+      checkOut: checkOutDate
+    });
+    
+    await reservation.save();
+    req.flash('success', 'Reservation ajoute avec succès');
+    return res.redirect('/catways/list');
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Erro ao obter a reserva.' });
+    console.log(error);
+    req.flash('error', 'Une erreur s\'est produite lors de l\'ajout de la reservation');
+    return res.redirect('/catways/list');
   }
 };
 
-// Criar uma nova reserva
-exports.createReservation = async (req, res) => {
+exports.deleteReservation = async (req, res) => {
+  const catwayNumber = req.params.id;
+  const idReservation = req.params.idReservation;
+
   try {
-    const reservation = await Reservation.create(req.body);
-    res.status(201).json(reservation);
+    const deletedReservation = await Reservation.findByIdAndDelete(idReservation);
+    if (!deletedReservation) {
+      req.flash('error', 'Cette reservation n\'existe pas');
+      return res.redirect('/catways/'+ catwayNumber + '/reservations');
+    }
+
+    const reservations = await Reservation.find({ catwayNumber: catwayNumber });
+    if (!reservations.length) {
+      req.flash('success', 'Réservation supprimé avec succès');
+      return res.redirect('/catways/list');
+    } else {
+      req.flash('success', 'Reservation supprimé avec succès');
+      return res.redirect('/catways/'+ catwayNumber + '/reservations');  
+    }
+    
+    
   } catch (error) {
-    res.status(400).json({ error: error.message });
+      req.flash('error', 'Une erreur s\'est produite lors de la suppression de la reservation');
+      return res.redirect('/catways/'+ catwayNumber + '/reservations');
+  }
+};
+
+exports.getReservationById = async (req, res) => {
+  const { id, idReservation } = req.params;
+
+  try{
+    const reservation = await Reservation.findOne({ catwayNumber: id, _id: idReservation });
+    if (!reservation) {
+      req.flash('error', 'Cette reservation n\'existe pas');
+      return res.redirect('/catways/'+ id + '/reservations');
+    }
+    return res.render('reservations/reservation', {
+      reservation: reservation,
+      path: '/reservations',
+      pageTitle: 'Reservation',
+      moment: moment
+    });
+  }
+  catch(error){
+    console.log(error);
+    req.flash('error', 'Une erreur s\'est produite lors de l\'obtention de la reservation');
+    return res.redirect('/catways/'+ id + '/reservations');
   }
 }
 
-// Apagar uma reserva existente
-exports.deleteReservationByCatwayAndId = async (req, res) => {
-  const { catwayNumber, idReservation } = req.params;
-  
-  try {
-    const result = await Reservation.deleteOne({
-      catwayNumber: catwayNumber,
-      _id: idReservation,
-    });
 
-    if (result.deletedCount === 0) {
-      return res.status(404).json({ message: 'Reserva não encontrada para este catway.' });
-    }
 
-    res.status(200).json({ message: 'Reserva apagada com sucesso.' });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Erro ao apagar a reserva.' });
-  }
-};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
